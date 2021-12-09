dd73b3e18a5707a4c36dbbdcc022f563550cfc9d
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-12-09 12:41:47 +1100

    Switch to UrlField from codem/silverstripe-html5-inputs

--- src/Forms/InlineLink_URLField.php
+++ src/Forms/InlineLink_URLField.php
@@ -1,21 +1,14 @@
 <?php
 
 namespace NSWDPC\InlineLinker;
-use BurnBright\ExternalURLField\ExternalURLField;
+use Codem\Utilities\HTML5\UrlField;
 
 /**
  * Allow a user to provide a URL for association with the link
  */
-class InlineLink_URLField extends ExternalURLField {
+class InlineLink_URLField extends UrlField {
 
     use InlineLink;
-
-    /**
-     * This is INPUT's type attribute value.
-     *
-     * @var string
-     */
-    protected $inputType = 'url';
 
     protected $link_type = InlineLinkField::LINKTYPE_URL;
 
